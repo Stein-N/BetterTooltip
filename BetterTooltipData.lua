78cd83127dd84b2640eee9ffaf03e2baa898437c
Stein-N <nico-stein-98@sbvl.de>
2025-09-04 09:38:45 +0200

    extract options to external lua and add langcodes

---
+++ BetterTooltipData.lua
@@ -0,0 +1,40 @@
+BetterTooltipData = {
+    toggleAnchor = {
+        ["key"] = "Tooltip_Toggle_Anchor",
+        ["default"] = false,
+        ["deDE"] = {
+            ["name"] = "Tooltip am Curser befestigen",
+            ["tooltip"] = "Alle Tooltips werden am Curser Befestigt, die Position kann verändert werden."
+        },
+        ["enEN"] = {
+            ["name"] = "Fixate Tooltip",
+            ["tooltip"] = "Fixate all Tooltips to the Cursor, the position can be changed."
+        }
+    },
+
+    anchorPosition = {
+        ["key"] = "Tooltip_Anchor_Position",
+        ["default"] = "ANCHOR_CURSOR_RIGHT",
+        ["deDE"] = {
+            ["name"] = "Tooltip Position",
+            ["tooltip"] = "Wähle die Position, an welcher der Tooltip befestigt wird."
+        },
+        ["enEN"] = {
+            ["name"] = "Tooltip position",
+            ["tooltip"] = "Choose the anchor position."
+        }
+    },
+
+    hideTooltips = {
+        ["key"] = "Tooltip_Hide",
+        ["default"] = false,
+        ["deDE"] = {
+            ["name"] = "Tooltip verstecken",
+            ["tooltip"] = "Versteckt alle Tooltips während des Kampfes"
+        },
+        ["enEN"] = {
+            ["name"] = "Hide Tooltips",
+            ["tooltip"] = "Hide all toolitps while fighting"
+        } 
+    }
+}
